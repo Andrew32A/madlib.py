2097f55edab1b9924c8f556deeb874e0142035b6
Andrew32A <111094607+Andrew32A@users.noreply.github.com>
2022-08-30 15:16:31 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,57 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+print("Welcome to madlibs!\n")
+
+name = input("Name: ")
+coding_language = input("Coding Language: ")
+made_up_coding_language = input("Made Up Coding Language: ")
+big_number = input("Big Number: ")
+small_number = input("Small Number: ")
+food = input("Food: ")
+caffeinated_drink = input("Caffeinated Drink: ")
+tech_company = input("Tech Company: ")
+
+print(f"\n{name}'s favorite coding language is {coding_language}.\n{name}s plan was to get hired at {tech_company} only armed with advanced {coding_language} knowledge.\n{name} believed that {coding_language} could be used for everything and anything.\n{name} refined and practiced for {big_number} years - only drinking {caffeinated_drink} and eating {food}.\nFinally feeling ready, {name} applied to {tech_company}.\nThe interviewer was greatly impressed with {name}'s portfollio.\nHowever, a new {coding_language} came out called {made_up_coding_language}.\n{made_up_coding_language} was able to shrink the entirety of {name}'s portfolio into {small_number} lines of code with ease.\nFeeling defeted, {name}'s went on for another {big_number} years to study {made_up_coding_language}.")
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
